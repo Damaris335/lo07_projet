20087480c17d9e70256308bb6e22e76ca1c9fd7b
damaris <damaris@damaris-ThinkPad-X1-Carbon-Gen-8>
2026-05-14 18:56:36 +0200

    test devisi et sql

--- index.php
+++ index.php
@@ -10,7 +10,42 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     </head>
     <body>
         <?php
-            echo 'Bonjour a toi et moi et voilà';
+            echo 'Test :';
+            
         ?>
+        <?php
+                        $dsn = 'mysql:dbname=barbotda;host=localhost;charset=utf8';
+                        $username = 'barbotda';
+                        $password = 'U6SngfE0';
+                        $options = array();
+                        try {
+                            $database = new PDO($dsn, $username, $password, $options);
+                        } catch (PDOException $e) {
+                            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+                        }
+                        ?>
+
+        
+        <?php
+        $requete2 = "select * from vin where annee = 1976";
+        $statement = $database->query($requete2);
+        ?>
+        <div class="container">
+            <div class="panel-group">
+                <div class="panel panel-success">
+                    <div class="mt-4 p-2 panel-heading text-white rounded bg-primary">Insertion d'un tuple : query = select * from vin where annee = 1976</div>
+                    <div class="panel-body">
+                        <?php
+                        $i = 1;
+                        // On utilise fetch() dans une boucle pour lire chaque ligne
+                        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                            echo $i . ". vin (" . $row['id'] . ", " . $row['cru'] . ", " . $row['annee'] . ", " . $row['degre'] . ")<br/>";
+                            $i++;
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
